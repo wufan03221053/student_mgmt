71844c79b6192cb212a2b4f3a59dc2be9985d037
wufan <wufan03221053@126.com>
2025-11-22 14:06:39 +0800

    实现小型学生管理系统，实现列表页面，学生信息的增删改查以及登录登出功能

---
+++ students/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('', views.student_list, name='student_list'),
+    path('add/', views.add_student, name='add_student'),
+    path('edit/<str:student_id>/', views.edit_student, name='edit_student'),
+    path('delete/<str:student_id>/', views.delete_student, name='delete_student'),
+]
